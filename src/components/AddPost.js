25df1f2a6e89b891f6956024e9cf667ba6f1a4cf
renetdevp <renetdevp@gmail.com>
2020-07-06 22:40:24 +0900

    0.0.9 insert onChange event to AddPost/form
    post 요청을 보내기 위한 data를 준비

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,24 +1,50 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
 
-import { fields, field } from '../styles/AddPost.css';
+import { fields, field, pullRight } from '../styles/AddPost.css';
+import { ADD_POST_REQUEST } from '../reducers/postsReducer';
 
 const AddPost = () => {
     const dispatch = useDispatch();
     const { addPostErrmsg } = useSelector((state) => state.post, []);
+    const [titleText, setTitleText] = useState('');
+    const [contentText, setContentText] = useState('');
+
+    const onValueChange = useCallback((e) => {
+        e.preventDefault();
+
+        switch (e.target.id){
+            case 'title':
+                setTitleText(e.target.value);
+                break;
+            case 'content':
+                setContentText(e.target.value);
+                break;
+            default:
+                return;
+        };
+    });
 
     const addPost = useCallback((e) => {
         e.preventDefault();
 
-        console.log('hos addpost');
+        console.log(`Title: ${titleText}, Content: ${contentText}`);
+        // dispatch({
+        //     type: ADD_POST_REQUEST,
+        //     data: {
+        //         title: titleText,
+        //         content: contentText
+        //     }
+        // });
     });
 
     return (
         <Form onSubmit={addPost}>
             <Form.Group className={fields}>
-                <Form.Input label="Title" placeholder="hos go?" className={field} />
-                <Form.TextArea label="Content" placeholder="hos gogo" className={field} />
+                <Form.Input label="Title" placeholder="hos go?" className={field} id="title" onChange={onValueChange} />
+                <Form.TextArea label="Content" placeholder="hos gogo" className={field} id="content" onChange={onValueChange} />
+                <Button type="submit" className={pullRight}>Add Post</Button>
             </Form.Group>
         </Form>
     );
